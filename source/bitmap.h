2afaecd8f4f6138f419aab24c689d64adb48da0b
Curtis Millar <curtis.millar@gmail.com>
2012-11-07 18:56:15 +1100

    v0.1.1

---
+++ source/bitmap.h
@@ -0,0 +1,26 @@
+/*
+ * bitmap.h
+ *
+ * Author:	Curtis Millar
+ * Date:	28 Oct 2012
+ *
+ * A library for working with bit-mapped images.
+ *
+ * Copyright (c) Curtis Millar 2012. All Rights Reserved.
+ */
+
+#ifndef HEADER_BITMAP
+#define HEADER_BITMAP
+
+#include "Color.h" //include the Color object
+#include "Canvas.h" //include the Bitmap object
+#include "format/BMP.h" //BMP file format
+#include "cmtools.h" //cmtools for various things
+
+//Standard constants
+#ifndef VOID
+#define VOID (-1)
+#endif
+#define BITMAP_ERROR (-1)
+
+#endif
